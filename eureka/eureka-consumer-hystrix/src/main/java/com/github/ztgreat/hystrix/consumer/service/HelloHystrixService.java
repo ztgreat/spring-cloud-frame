8b12343c310492b7acaf998186118101d67d7d5c
小张 <1170881778@qq.com>
2019-01-23 15:27:40 +0800

    hystrix

--- eureka/eureka-consumer-hystrix/src/main/java/com/github/ztgreat/hystrix/consumer/service/HelloHystrixService.java
+++ eureka/eureka-consumer-hystrix/src/main/java/com/github/ztgreat/hystrix/consumer/service/HelloHystrixService.java
@@ -1,10 +1,14 @@
 package com.github.ztgreat.hystrix.consumer.service;
 
-import com.github.ztgreat.hystrix.consumer.service.fallback.HelloHystrixFallbackService;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
+/**
+ * 对于查询操作，我们可以实现一个 fallback 方法，当请求后端服务出现异常的时候，可以使用 fallback 方法返回的值。
+ * fallback 方法的返回值一般是设置的默认值或者来自缓存。
+ */
 @FeignClient(name = "client01",fallback = com.github.ztgreat.hystrix.consumer.service.fallback.HelloHystrixFallbackService.class )
 public interface HelloHystrixService {
 
