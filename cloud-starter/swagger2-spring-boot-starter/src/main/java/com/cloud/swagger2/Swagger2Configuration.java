daf3d5569a006e2e1cbfe9a8c996c8bea8542df8
ztgreat <1170881778@qq.com>
2020-03-07 14:10:47 +0800

    结构调整，swagger

---
+++ cloud-starter/swagger2-spring-boot-starter/src/main/java/com/cloud/swagger2/Swagger2Configuration.java
@@ -0,0 +1,14 @@
+package com.cloud.swagger2;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.documentation.swagger2.configuration.Swagger2DocumentationConfiguration;
+
+@Configuration
+@ConditionalOnProperty(name = "swagger.enabled", matchIfMissing = true)
+@Import({
+        Swagger2DocumentationConfiguration.class
+})
+public class Swagger2Configuration {
+}
